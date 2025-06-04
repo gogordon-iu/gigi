837a23c5d4f2d4cb4ca9b5a8eb1a7b738a1402f2
curiosityeducation <gogordon@iu.edu>
2025-06-04 16:12:54 -0400

    Update testActivity.py

--- Scripts/testActivity.py
+++ Scripts/testActivity.py
@@ -9,22 +9,6 @@ from characterDefinitions import CHARACTER_FOLDER
  
 teacher_path = "../Assets/teacher/"
 
-# Introduction sequence
-# 
-# Hi. I am Gigi [wave hello]. 
-# I am a social robot and I want to help children learn.
-# I can do lots of things, like look at you.
-# I can also perform gestures, like these [arms wide open].
-# If I need to show things, I can hide my face and present what is needed instead, like this.
-# [show image]
-# [laugh] That was funny, me showing me [smile].
-# I'm also a very good listener. Do you want to see, answer with yes or no?
-# [yes, no] -- > you said [yes,no]. [laughs] I just demonstrated that I can hear you.
-# Enough about what I can do, let's talk about how I came to be.
-# One of the special things about me, is that you, or your students, can build me, or social robots like me.
-# Hopefully, today, we will work together towards learning how to do that.
-# Goren, do you want to say a few words?
-
 activity_name = "test activity"
 
 class TestActivity(ScriptGraph) : 
@@ -37,16 +21,37 @@ class TestActivity(ScriptGraph) :
                             text="Hi, I am Gigi. This is a test activity.",
                             motors="wave_hello")
        
-        # self.graph.add_node("qr_01", type="find", 
-        #                     what="qr", timeout=5)
-        # self.graph.add_edge("start", "qr_01", label="start find")
+        self.graph.add_node('Node_14', type=['find'], what='qr', timeout=60, 
+                            data=['child number 1', 'child number 2', 'child number 3', 'child number 4'])
+        self.graph.add_node('Node_15', type=['speak'], text='Let me know when you are done by raising the card.')
+        self.graph.add_edge('Node_14', 'Node_15', label='no')
+        self.graph.add_edge('Node_15', 'Node_14', label='Node_15_14')
+        self.graph.add_node('Node_16', type=['speak'], text='Excellent!')
+        self.graph.add_edge('Node_14', 'Node_16', label='yes')
 
-        # self.graph.add_node("qr_02", type="speak", 
-        #                     text="Here you are.")
-        # self.graph.add_edge("qr_01", "qr_02", label="qr_01_02")
+        self.graph.add_node('Node_114', type=['find'], what='qr', timeout=60, 
+                            data=['child number 1', 'child number 2', 'child number 3', 'child number 4'])
+        self.graph.add_node('Node_115', type=['speak'], text='Let me know when you are done by raising the card.')
+        self.graph.add_edge('Node_114', 'Node_115', label='no')
+        self.graph.add_edge('Node_115', 'Node_114', label='Node_15_14')
+        self.graph.add_node('Node_116', type=['speak'], text='Excellent!')
+        self.graph.add_edge('Node_114', 'Node_116', label='yes')
+
+        self.graph.add_node('Node_214', type=['find'], what='qr', timeout=60, 
+                            data=['child number 1', 'child number 2', 'child number 3', 'child number 4'])
+        self.graph.add_node('Node_215', type=['speak'], text='Let me know when you are done by raising the card.')
+        self.graph.add_edge('Node_214', 'Node_215', label='no')
+        self.graph.add_edge('Node_215', 'Node_214', label='Node_15_14')
+        self.graph.add_node('Node_216', type=['speak'], text='Excellent!')
+        self.graph.add_edge('Node_214', 'Node_216', label='yes')
+
+        self.graph.add_edge('start', 'Node_14', label='Node_15_14')
+        self.graph.add_edge('Node_16', 'Node_114', label='Node_15_14')
+        self.graph.add_edge('Node_116', 'Node_214', label='Node_15_14')
+        self.graph.add_edge('Node_216', 'The End', label='finished')
 
         self.graph.add_node("The End", type="end")
-        self.graph.add_edge("start", "The End", label="finished")
+        # self.graph.add_edge("start", "The End", label="finished")
 
         
 if __name__ == "__main__":
