892990555770910c57e8fdde6e6657d0274ea617
curiosityeducation <gogordon@iu.edu>
2025-06-08 11:18:21 -0400

    Update wakeup_gigi.sh

--- Setup/wakeup_gigi.sh
+++ Setup/wakeup_gigi.sh
@@ -8,6 +8,17 @@ xset s off
 xset -dpms
 xset s noblank
 
+SOURCE="/home/orangepi/Code/gigi/Character/motorData_calibrated_local.json"
+DEST="/home/orangepi/Code/gigi/Character/motorData_calibrated.json"
+
+if [ -f "$SOURCE" ]; then
+    cp "$SOURCE" "$DEST"
+    echo "File copied to $DEST"
+else
+    echo "Source file does not exist: $SOURCE"
+fi
+
+
 cd /home/orangepi/Code/gigi
 chmod +x activate_environment.sh
 ./activate_environment.sh
