610b919af246334761e61a65a5ba0de3465c3fdd
curiosityeducation <gogordon@iu.edu>
2025-10-31 09:49:01 -0400

    updated halloween

--- Character/character.py
+++ Character/character.py
@@ -208,9 +208,9 @@ class Character():
     def lookat_something(self, what="face", timeout=-1):
         # timeout - how long (seconds) to look for a face if one is not found
         start_time = time.time()
-        if self.vision:
-            self.vision.look_and_stop(what=what, timeout=1)
+        if self.vision:            
             if self.lookat_calibration:     # if calibrated, look at something
+                self.vision.look_and_stop(what=what, timeout=1)
                 print("DEBUG: ", what, self.vision.found)                
                 if len(self.vision.found[what]) > 0:
                     self.vision.stop_vision()
