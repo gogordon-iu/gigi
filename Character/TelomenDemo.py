12a1c6497819fb18c7f8bd01e3df1cb7a97ca351
curiosityeducation <gogordon@iu.edu>
2025-11-12 20:31:32 -0500

    Create TelomenDemo.py

---
+++ Character/TelomenDemo.py
@@ -0,0 +1,15 @@
+from character import Character
+
+# Lookf_for face = Stephanie
+# when detect, say "Hello Stephanie, how are you today?"
+# then wait for response
+# reply to the response. Then say "Goren, are You happy to be here?"
+# wait for a thumbs up
+# reply: I hope you have a great collaboration together. Bye bye!
+
+# initialize character
+
+fuzzy = Character(child=False, gender='female', activity='TelomenDemo', languages=['en'])
+
+
+# use vision
