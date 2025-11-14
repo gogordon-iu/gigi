29661883637b56da6b8d59a235f05438d0996199
curiosityeducation <gogordon@iu.edu>
2025-11-13 23:25:53 -0500

    Update face.py

--- Character/face.py
+++ Character/face.py
@@ -16,9 +16,9 @@ elif IMAGE_OPTION == "cv":
 import time
 import threading
 
-import face_recognition
+# import face_recognition
 import mediapipe as mp
-from deepface import DeepFace
+# from deepface import DeepFace
 
 
 class Face():
