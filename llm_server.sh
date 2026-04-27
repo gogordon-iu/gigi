1cd6e0b32fdd66c1374ec30f63aded7593f4eb81
gowthyreddy <gowtham.rdy2001@gmail.com>
2026-04-27 19:09:45 -0400

    updated llm_server

--- llm_server.sh
+++ llm_server.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-python3 /home/orangepi/Code/gigi/Resources/rknn-llm/examples/rkllm_server_demo/rkllm_server/flask_server.py --rkllm_model_path /home/orangepi/Code/gigi/Resources/qwen2.5_1.5b.rkllm --target_platform rk3588
+cd /home/orangepi/Code/gigi/Resources/rknn-llm/examples/rkllm_server_demo/rkllm_server/
+python3 flask_server.py --rkllm_model_path /home/orangepi/Code/gigi/Resources/qwen2.5_1.5b.rkllm --target_platform rk3588
