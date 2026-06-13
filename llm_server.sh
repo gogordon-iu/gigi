c7d43e7bd51e3ede0b5fc9e231bc00282a7727a4
curiosityeducation <gogordon@iu.edu>
2026-06-13 17:05:39 -0400

    npu: revert model to qwen2.5_1.5b to avoid NPU memory allocation failure

--- llm_server.sh
+++ llm_server.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd /home/orangepi/Code/gigi/Resources/rknn-llm/examples/rkllm_server_demo/rkllm_server/
-/home/orangepi/Code/gigi/.venv/bin/python3 flask_server.py --rkllm_model_path /home/orangepi/Code/gigi/Resources/qwen2.5_3b.rkllm --target_platform rk3588
+/home/orangepi/Code/gigi/.venv/bin/python3 flask_server.py --rkllm_model_path /home/orangepi/Code/gigi/Resources/qwen2.5_1.5b.rkllm --target_platform rk3588
